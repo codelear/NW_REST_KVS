85f36dcaa70804e6ae35a9e50eaeff8875644f12
codelear <39409007+codelear@users.noreply.github.com>
2018-05-18 19:41:36 +0530

    Create SocketServer.h

---
+++ SocketServer.h
@@ -0,0 +1,7 @@
+#include <string>
+
+std::string GET_RESPONSE_JSON="HTTP/1.1 200 OK\r\nCache-Control: private\r\nContent-Type: application/json\r\n";
+char GET_VERB[4]="GET";
+char PUT_VERB[4]="PUT";
+char DELETE_VERB[7]="DELETE";
+
